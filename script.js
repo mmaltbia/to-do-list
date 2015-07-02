a28f811a902145e4b0949c2afecad928698acaec
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-07-02 13:02:04 -0700

    OOP Refactor

--- script.js
+++ script.js
@@ -1,79 +1,153 @@
 $(document).ready(function(){
 
-// Array that holds each task, description of task, and due date
-var toDoArr = [
-
-	{name: "Pick up dry cleaning",
-	description: "By 5 P.M."},
-
-	{name: "Wash Car",
-	description: "By 10 A.M."},
-
-	{name: "Pack for Vacation",
-	description: "By 8 A.M."}
-];
-
-// Form to create new task 
-var $form = $('#new_todo_item');
-
-
-// Var for task list
-var $taskListUl = $("#task-list-ul");
-
- // append existing todos (from seed data) to `$toDoList`
-  // `_.each` is an "iterator" function provided by Underscore.js
-_.each(toDoArr, function (task, index) {
-  $taskListUl.append('<li class="item"><p>' + task.name + ' - ' + task.description + '</p></li>');
-});
-
- // submit form to create new todo
-$form.on('submit', function(name, description){
-	event.preventDefault();
-	var $name = $('#form-input-task').val();
-	var $description = $('#form-input-description').val();
-	var $taskData = {name: $name, description: $description};
-	toDoArr.push($taskData);
-	$taskListUl.append('<li class="item"><p>' + $name + ' - ' + $description + '</p></li>');
-	$form[0].reset();
-})
-
-
 //Full Width Background Image
 $.backstretch("images/notebook2.jpg");
 
-// Var for task list
+function ToDo(name, description){
+	this.name = name;
+	this.description = description;
+}
+
+var toDo = new ToDo(name, description);
+
+ToDo.all = [];
+
+
+ToDo.prototype.save = function(){
+	ToDo.all.push(this);
+	console.log(ToDo.all);
+}
+
+console.log(ToDo.all[0]);
+
+//Underscore Template
+var underscoreTemplate = _.template($('#underscore-template').html());
+
 var $taskListUl = $("#task-list-ul");
 
-// Var for the add to list button 
-var $addBtn = $("#add-btn");
+ToDo.prototype.render = function (name, description){
+	$(underscoreTemplate(this));
 
-// Var for show completed button 
-var $showCompletedBtn = ("#show-completed");
 
-console.log($('#underscore-template').html());
+}
+ // append existing todos (from seed data) to `$toDoList`
+ // `_.each` is an "iterator" function provided by Underscore.js
 
-// Var for underscore template
-var $underscoreTemplate = _.template($('#underscore-template').html());
 
-// Var for task list in template portion of HTM
-var $taskListTemplate = $("#task-list-template");
 
-// Var for task and description input fields
-var $formInputTask = $("#form-input-task");
+
+var $form = $('#new_todo_item')
+var $formInputName = $("#form-input-name");
 var $formInputDescription = $("form-input-description");
 var $li = $("li");
 
-$taskListUl.on("click", ".item", function(){
-	$(this).addClass("done");
 
-	$(this).css('text-decoration', 'line-through')
-	$(this).fadeOut('slow', 0);
-	completedItems.push(this);
-	console.log(completedItems);
+
+
+ // submit form to create new todo
+$form.on('submit', function(){
+		event.preventDefault();
+	// if ($formInputName.val() !== null && $formInputDescription.val() !== null){
+		// var newTask = new ToDo($formInputName.val(), $formInputDescription.val());
+		var newTask = new ToDo($formInputName.val(), $formInputDescription.val());
+		newTask.save();
+		$taskListUl.append('<li class="item"><p>' + ToDo.all[i].name + ' - ' + ToDo.all[i].description + '</p></li>');
+		$form[0].reset();
+		console.log('This is working');		
+
+	// var $name = $('#form-input-task').val();
+	// var $description = $('#form-input-description').val();
+	// ToDo.all.push($taskData);
+
+	newTask.render();
 })
+
+console.log("Goodbye");
+
+_.each(ToDo.all, function (task, index) {
+	task.render();
+  	$taskListUl.append('<li class="item"><p>' + task.name + ' - ' + task.description + '</p></li>');
+});
+
+})
+
+
+//!!!!Script from Underscore Edit!!!! - This works!
+
+// // Array that holds each task, description of task, and due date
+// var toDoArr = [
+
+// 	{name: "Pick up dry cleaning",
+// 	description: "By 5 P.M."},
+
+// 	{name: "Wash Car",
+// 	description: "By 10 A.M."},
+
+// 	{name: "Pack for Vacation",
+// 	description: "By 8 A.M."}
+// ];
+
+// // Form to create new task 
+// var $form = $('#new_todo_item');
+
+
+// // Var for task list
+// var $taskListUl = $("#task-list-ul");
+
+//  // append existing todos (from seed data) to `$toDoList`
+//   // `_.each` is an "iterator" function provided by Underscore.js
+// _.each(toDoArr, function (task, index) {
+//   $taskListUl.append('<li class="item"><p>' + task.name + ' - ' + task.description + '</p></li>');
+// });
+
+//  // submit form to create new todo
+// $form.on('submit', function(name, description){
+// 	event.preventDefault();
+// 	var $name = $('#form-input-task').val();
+// 	var $description = $('#form-input-description').val();
+// 	var $taskData = {name: $name, description: $description};
+// 	toDoArr.push($taskData);
+// 	$taskListUl.append('<li class="item"><p>' + $name + ' - ' + $description + '</p></li>');
+// 	$form[0].reset();
+// })
+
+
+// //Full Width Background Image
+// $.backstretch("images/notebook2.jpg");
+
+// // Var for task list
+// var $taskListUl = $("#task-list-ul");
+
+// // Var for the add to list button 
+// var $addBtn = $("#add-btn");
+
+// // Var for show completed button 
+// var $showCompletedBtn = ("#show-completed");
+
+// console.log($('#underscore-template').html());
+
+// // Var for underscore template
+// var $underscoreTemplate = _.template($('#underscore-template').html());
+
+// // Var for task list in template portion of HTM
+// var $taskListTemplate = $("#task-list-template");
+
+// // Var for task and description input fields
+// var $formInputTask = $("#form-input-task");
+// var $formInputDescription = $("form-input-description");
+// var $li = $("li");
+
+// $taskListUl.on("click", ".item", function(){
+// 	$(this).addClass("done");
+
+// 	$(this).css('text-decoration', 'line-through')
+// 	$(this).fadeOut('slow', 0);
+// 	completedItems.push(this);
+// 	console.log(completedItems);
+// })
  	
 
-})
+// })
 
 
 
